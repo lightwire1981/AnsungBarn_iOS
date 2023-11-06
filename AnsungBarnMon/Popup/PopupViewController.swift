cb9837350c9fe533d0cdd7c9ff99fe437f888b71
lightwire1981 <mintael@gmail.com>
2023-11-06 15:23:55 +0900

    Login button logic & ui Modified

---
+++ AnsungBarnMon/Popup/PopupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PopupViewController.swift
+//  AnsungBarnMon
+//
+//  Created by 센코 on 10/31/23.
+//
+
+import UIKit
+
+class PopupViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AnsungBarnMon/Util/Reachability.swift
@@ -0,0 +1,8 @@
+//
+//  Reachability.swift
+//  AnsungBarnMon
+//
+//  Created by 센코 on 11/1/23.
+//
+
+import Foundation
